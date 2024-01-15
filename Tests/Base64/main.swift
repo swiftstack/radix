83757dd006d365d7e4fda14daa3c4907708d50aa
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-15 06:58:03 +0300

    [Tests] Reflect API changes

--- Tests/Base64/main.swift
+++ Tests/Base64/main.swift
@@ -3,13 +3,13 @@ import Test
 
 // MARK: Correctness
 
-test.case("encode string to string") {
+test("encode string to string") {
     expect(String(encodingToBase64: "swiftstack") == "c3dpZnRzdGFjaw==")
     expect(String(encodingToBase64: "swiftstack.") == "c3dpZnRzdGFjay4=")
     expect(String(encodingToBase64: "swiftstack..") == "c3dpZnRzdGFjay4u")
 }
 
-test.case("decode string to string") {
+test("decode string to string") {
     expect(String(decodingBase64: "c3dpZnRzdGFjaw==") == "swiftstack")
     expect(String(decodingBase64: "c3dpZnRzdGFjay4=") == "swiftstack.")
     expect(String(decodingBase64: "c3dpZnRzdGFjay4u") == "swiftstack..")
@@ -22,28 +22,28 @@ let decodedBytes: [UInt8] = .init(decodedString.utf8)
 let encodedString: String = "c3dpZnRzdGFjaw=="
 let encodedBytes: [UInt8] = .init(encodedString.utf8)
 
-test.case("encode string to bytes") {
+test("encode string to bytes") {
     expect([UInt8](encodingToBase64: decodedString) == encodedBytes)
 }
 
-test.case("decode string to bytes") {
+test("decode string to bytes") {
     expect([UInt8](decodingBase64: encodedString) == decodedBytes)
 }
 
-test.case("encode bytes to string") {
+test("encode bytes to string") {
     expect(String(encodingToBase64: decodedBytes) == encodedString)
 }
 
-test.case("decode bytes to string") {
+test("decode bytes to string") {
     expect(String(decodingBase64: encodedBytes) == decodedString)
 }
 
-test.case("encode bytes to bytes") {
+test("encode bytes to bytes") {
     expect([UInt8](encodingToBase64: decodedBytes) == encodedBytes)
 }
 
-test.case("decode bytes to bytes") {
+test("decode bytes to bytes") {
     expect([UInt8](decodingBase64: encodedBytes) == decodedBytes)
 }
 
-test.run()
+await run()
--- Tests/Hex/main.swift
+++ Tests/Hex/main.swift
@@ -3,30 +3,30 @@ import Test
 
 let bytes: [UInt8] = [192, 1]
 
-test.case("encode to hex") {
+test("encode to hex") {
     expect(String(encodingToHex: bytes) == "c001")
 }
 
-test.case("encode to hex uppercased") {
+test("encode to hex uppercased") {
     expect(String(encodingToHex: bytes, format: .uppercase) == "C001")
 }
 
-test.case("encode to hex array") {
+test("encode to hex array") {
     expect(String(encodingToHex: [], format: .array) == "[]")
     expect(String(encodingToHex: bytes, format: .array) == "[0xc0, 0x01]")
 }
 
-test.case("encode to hex uppercased array") {
+test("encode to hex uppercased array") {
     let result = String(encodingToHex: bytes, format: [.uppercase, .array])
     expect(result == "[0xC0, 0x01]")
 }
 
-test.case("decode from hex") {
+test("decode from hex") {
     expect([UInt8](decodingHex: "c001") == bytes)
 }
 
-test.case("decode from hex uppercased") {
+test("decode from hex uppercased") {
     expect([UInt8](decodingHex: "C001") == bytes)
 }
 
-test.run()
+await run()
